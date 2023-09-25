50b053a370ec08fb52e7f7ccd507bf02021e2c62
sveta-1970 <ssarisoydan@gmail.com>
2023-09-25 14:28:27 +0300

    Added promo component, upd header&project&app css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./app.css";
 import Header from "../header/header";
+import Promo from "../promo/promo";
 import Project from "../project/project";
 import Footer from "../footer/footer";
 
@@ -8,7 +9,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className="promo"></div>
+      <div className="promo">
+        <Promo />
+        <Promo />
+        <Promo />
+        <Promo />
+      </div>
       <h2>Наші найбільші проекти</h2>
       <div className="projects">
         <Project />
---
+++ src/components/promo/promo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./promo.css";
+
+function Promo() {
+  return (
+    <div className="Promo">
+      <p>26</p>
+      <p>років</p>
+      <p>на ринку</p>
+    </div>
+  );
+}
+
+export default Promo;
